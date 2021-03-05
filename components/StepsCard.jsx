2ac268f29bde2226ec84ad8507baf8fe9db72fb0
lilcser <ninapan@Ninas-MacBook-Pro-2.local>
2021-03-05 16:53:22 -0500

    add structure

---
+++ components/StepsCard.jsx
@@ -0,0 +1,29 @@
+export default function stepsCaard() {
+    return (
+      <div className="m-10">
+        <h className="block text-black">Steps to take the Vaccine</h>
+        <div className="grid grid-cols-5">
+          <div className="m-4 h-full inline-block rounded bg-brand-blurple-light">
+            <div className="felx felx-col">
+              <div className="justify-between">
+                <div className="mx-5 my-4 text-brand-blurple font-bold">STEP 1</div>
+                <div className="mx-5 my-4 text-black">Pre-register for the vaccine at your nearest hospital.</div>
+                <div className="m-2 p-4 text-brand-blurple font-bold border-4 border-brand-blurple rounded bg-transparent">LEARN MORE</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="m-4 h-full inline-block rounded bg-brand-blurple-light">
+          </div>
+
+          <div className="m-4 inline-block rounded bg-brand-blurple-light">
+          </div>
+          <div className="m-4 inline-block rounded bg-brand-blurple-light">
+          </div>
+          <div className="m-4 inline-block rounded bg-brand-blurple-light">
+          </div>
+        </div>
+      </div>
+    )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import withLayout from '../components/withLayout'
 import NeedToKnow from '../components/NeedToKnow'
+import MapCanadaVaccines from '../components/MapCanadaVaccines'
+import QuizOption from '../components/QuizOption'
+import StepsCard from '../components/StepsCard'
+// <MapCanadaVaccines />
 
 function Home() {
   return (
     <main>
       <NeedToKnow />
+      <div className="w-1/2 mx-auto space-y-4">
+        {[1, 2, 3, 4].map((i) => (
+          <QuizOption key={i} option={i} />
+        ))}
+      </div>
+
+      <StepsCard />
     </main>
   )
 }
