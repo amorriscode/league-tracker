46021e40aaaa86b99458ed4dc2fdd436ba7f99e4
Anthony Morris <amorriscode@gmail.com>
2021-03-04 18:35:35 -0800

    Add brand colours and a layout component

--- components/QuizOption.jsx
+++ components/QuizOption.jsx
@@ -1,7 +1,7 @@
 export default function QuizOption({ option }) {
   return (
     <div className="p-4 bg-green-600 hover:bg-green-800 text-white rounded">
-      {option}
+      {option} Hello
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Noto Sans', 'sans-serif'],
+    },
     extend: {
-      colors: {},
+      colors: {
+        'brand-blurple-light': '#EDE8FA',
+        'brand-blurple-med': '#B9A4ED',
+        'brand-blurple': '#501CD2',
+        'brand-teal-light': '#E8F8F5',
+        'brand-teal': '#01C3A2',
+        'brand-teal-dark': '#00A886',
+        'brand-blue-light': '#EAEEFB',
+        'brand-blue': '#2E5CDF',
+        'brand-red-light': '#F9DEE3',
+        'brand-red': '#D92045',
+        'brand-red-dark': '#BA1B3B',
+      },
     },
   },
   variants: {
