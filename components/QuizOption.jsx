fa4e08955b03562f69609910668437a8285b63dd
Michael Gesuale <mikeg_107@hotmail.com>
2021-03-05 17:03:42 -0500

    Quiz options styles (#9)
    * quizOptions buttons
    
    * quiz
    
    * Refactor quiz stuff
    
    Co-authored-by: Anthony Morris <amorriscode@gmail.com>

--- components/QuizOption.jsx
+++ components/QuizOption.jsx
@@ -1,7 +1,13 @@
-export default function QuizOption({ option }) {
+export default function QuizOption({ option, selected, onHandleSelect }) {
   return (
-    <div className="p-4 bg-green-600 hover:bg-green-800 text-white rounded-lg">
-      {option} Hello
+    <div
+      className={`p-2 rounded-lg border border-brand-blue bg-white w-full hover:border-opacity-75 hover:cursor-pointer ${
+        selected ? 'bg-brand-blue text-white' : ''
+      }`}
+      onClick={onHandleSelect}
+    >
+      <div className="m-1 pt-1 float-left rounded-full h-4 w-4 flex items-center justify-center bg bg-button-gray" />
+      <div className="flex pl-2">{option}</div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import withLayout from '../components/withLayout'
 import NeedToKnow from '../components/NeedToKnow'
+import Survey from '../components/Survey'
 
 function Home() {
   return (
     <main>
+      <Survey />
       <NeedToKnow />
     </main>
   )
