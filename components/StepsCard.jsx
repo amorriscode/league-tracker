e50ee0c96b9f30e947462d949b6db1d7987d621b
lilcser <ninapan@Ninas-MacBook-Pro-2.local>
2021-03-05 17:16:27 -0500

    add details and change ui for stepscard

--- components/StepsCard.jsx
+++ components/StepsCard.jsx
@@ -1,29 +1,43 @@
+import Section from './Section'
+
 export default function stepsCaard() {
     return (
-      <div className="m-10">
-        <h className="block text-black">Steps to take the Vaccine</h>
-        <div className="grid grid-cols-5">
-          <div className="m-4 h-full inline-block rounded bg-brand-blurple-light">
+      <Section title="Steps to take the Vaccine">
+        <div className="grid grid-cols-5 gap-8">
+          <div className="relative rounded-lg bg-brand-blurple-light">
             <div className="felx felx-col">
-              <div className="justify-between">
-                <div className="mx-5 my-4 text-brand-blurple font-bold">STEP 1</div>
-                <div className="mx-5 my-4 text-black">Pre-register for the vaccine at your nearest hospital.</div>
-                <div className="m-2 p-4 text-brand-blurple font-bold border-4 border-brand-blurple rounded bg-transparent">LEARN MORE</div>
-              </div>
+              <div className="mx-5 my-4 text-brand-blurple font-bold">STEP 1</div>
+              <div className="mx-5 my-4 text-black">Pre-register for the vaccine at your nearest hospital.</div>
+              <div className="mx-3 my-2 p-4 text-brand-blurple font-bold border-4 border-brand-blurple rounded bg-transparent">LEARN MORE</div>
             </div>
           </div>
 
-          <div className="m-4 h-full inline-block rounded bg-brand-blurple-light">
+          <div className="relative rounded-lg bg-brand-blurple-light">
+              <div className="mx-5 my-4 text-brand-blurple font-bold">STEP 2</div>
+              <div className="mx-5 my-4 text-black">You’re registered and have an appointment confirmation now, what’s next?</div>
+              <div className="mx-3 my-2 p-4 text-brand-blurple font-bold border-4 border-brand-blurple rounded bg-transparent">LEARN MORE</div>
           </div>
 
-          <div className="m-4 inline-block rounded bg-brand-blurple-light">
+          <div className="relative rounded-lg bg-brand-blurple-light">
+              <div className="mx-5 my-4 text-brand-blurple font-bold">STEP 3</div>
+              <div className="mx-5 my-4 text-black">It’s the day of the vaccine! Learn more about what to expect on the day of.</div>
+              <div className="mx-3 my-2 p-4 text-brand-blurple font-bold border-4 border-brand-blurple rounded bg-transparent">LEARN MORE</div>
           </div>
-          <div className="m-4 inline-block rounded bg-brand-blurple-light">
+
+          <div className="relative rounded-lg bg-brand-blurple-light">
+              <div className="mx-5 my-4 text-brand-blurple font-bold">STEP 4</div>
+              <div className="mx-5 my-4 text-black">You just took your first vaccine. Here’s what to expect before your second dose.</div>
+              <div className="mx-3 my-2 p-4 text-brand-blurple font-bold border-4 border-brand-blurple rounded bg-transparent">LEARN MORE</div>
           </div>
-          <div className="m-4 inline-block rounded bg-brand-blurple-light">
+
+          <div className="relative rounded-lg bg-brand-blurple-light">
+              <div className="mx-5 my-4 text-brand-blurple font-bold">STEP 5</div>
+              <div className="mx-5 my-4 text-black">You just took your first vaccine. Here’s what to expect before your second dose.</div>
+              <div className="mx-3 my-2 p-4 text-brand-blurple font-bold border-4 border-brand-blurple rounded bg-transparent">LEARN MORE</div>
           </div>
+
         </div>
-      </div>
+      </Section>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,16 @@ import MapCanadaVaccines from '../components/MapCanadaVaccines'
 import QuizOption from '../components/QuizOption'
 import StepsCard from '../components/StepsCard'
 // <MapCanadaVaccines />
+// <div className="w-1/2 mx-auto space-y-4">
+//   {[1, 2, 3, 4].map((i) => (
+//     <QuizOption key={i} option={i} />
+//   ))}
+// </div>
 
 function Home() {
   return (
     <main>
       <NeedToKnow />
-      <div className="w-1/2 mx-auto space-y-4">
-        {[1, 2, 3, 4].map((i) => (
-          <QuizOption key={i} option={i} />
-        ))}
-      </div>
-
       <StepsCard />
     </main>
   )
